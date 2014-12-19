cd11f8ce29de19fa91c7a246b5a5a26d9997310d
Andrew Jackson <a.jackson@tcd.ie>
2014-12-19 10:44:35 +0000

    fixing a conflict

--- R/plot.siber.object.R
+++ R/plot.siber.object.R
@@ -8,14 +8,7 @@ plot.siber.object <- function(siber,
                               bty = "L", 
                               xlab = "Isotope 1", 
                               ylab = "Isotope 2",
-=======
-plot.siber.object <- function(siber, iso.order = c(1,2), 
-                              ax.pad = 1, hulls = T, ellipses = T,
-                              group.hulls = F, bty = "L", 
-                              xlab = "x",
-                              ylab = "y",
                               las = 1,
->>>>>>> Stashed changes
                               ...){
 
   # NOTE - this isotope ordering needs to be passed onwards to the plotting
@@ -33,16 +26,10 @@ plot.siber.object <- function(siber, iso.order = c(1,2),
   		       	        siber$iso.summary["max", x] + ax.pad ),
   		       ylim = c(siber$iso.summary["min", y] - ax.pad , 
   		       	        siber$iso.summary["max", y] + ax.pad ),
-<<<<<<< Updated upstream
-             xlab = xlab, 
-             ylab = ylab, 
-             bty = bty
-=======
              ylab = ylab,
              xlab = xlab,
              bty = bty,
              las = las
->>>>>>> Stashed changes
              
   		)
 
@@ -57,20 +44,13 @@ plot.siber.object <- function(siber, iso.order = c(1,2),
     	
     }
 
-    # ==========================================================================
-  	# I might move this plotting block out to their own functions in the future
-    # This will be easy since they only require object siber to be passed.
-    # It should also make passing arguments specific to each plot-type
-    # more straight forward, through this wrapper function and onwards.
-    # The challenge is that they should be allowed to take a list of different
-    # options for each plot. How best to pass theses on i dont know. 
-    # I could just pass in a list, and then un-pack it.
-  	# ==========================================================================
+
 
     # --------------------------------------------------------------------------
     # Add a convex hull between the means of each group, i.e. a convex hull
     # for the community. Only applicable if there are more than 2 
     # members for a community
+    # I might move this out block to its own function in the future
     # --------------------------------------------------------------------------
     if (hulls) {
       # NOTE currently not working
@@ -91,10 +71,6 @@ plot.siber.object <- function(siber, iso.order = c(1,2),
       # code similar to group ellipses to go here
       plot.group.hulls(siber, group.hull.args)
     } # end of if statement for group hull drawing
-    
-  	# ==========================================================================
-    # END OF PLOTTING BLOCK
-  	# ==========================================================================
     
   }) # end of with() function
 
